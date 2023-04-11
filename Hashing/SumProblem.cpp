35d69c14a48b08fcca3620a5bc2bb564ffe295bb
FarhanNanoCoder <farhan.shariar.094@gmail.com>
2023-04-11 22:05:43 +0600

    feat: tow sum

---
+++ Hashing/SumProblem.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVector(vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+}
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    map<int, int> m;
+    vector<int> res;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int remain = target - nums[i];
+        if (m.find(remain) != m.end())
+        {
+            res.push_back(i);
+            res.push_back(m[remain]);
+        }
+        else
+        {
+            m[nums[i]] = i;
+        }
+    }
+    return res;
+}
+int main()
+{
+    vector<int> nums={2,7,11,15};
+    vector<int> res = twoSum(nums,9);
+    printVector(res);
+}
