d477965c00951d04e9c98b300489f40b54d741f0
FarhanNanoCoder <farhan.shariar.094@gmail.com>
2023-10-05 20:30:25 +0600

    feat: max-area

---
+++ Blind75/Graph/max-area.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int r, int c, vector<vector<bool>> &visited, vector<vector<int>> &grid, int ROW, int COL, int &area)
+{
+    if (r >= ROW || c >= COL || r < 0 || c < 0 || visited[r][c] || grid[r][c] == 0)
+        return;
+    area++;
+    visited[r][c] = true;
+    dfs(r + 1, c, visited, grid, ROW, COL, area);
+    dfs(r - 1, c, visited, grid, ROW, COL, area);
+    dfs(r, c + 1, visited, grid, ROW, COL, area);
+    dfs(r, c - 1, visited, grid, ROW, COL, area);
+}
+
+int maxAreaOfIsland(vector<vector<int>> &grid)
+{
+    if (grid.empty())
+        return 0;
+    vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+    int area = 0;
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[i].size(); j++)
+        {
+            if (!visited[i][j] && grid[i][j] == 1)
+            {
+                int temp = 0;
+                dfs(i, j, visited, grid, grid.size(), grid[i].size(), temp);
+                area = max(area, temp);
+            }
+        }
+    }
+    return area;
+}
+int main()
+{
+    vector<vector<int>> grid = {{1, 1, 0, 0, 0},
+                                {1, 1, 0, 1, 0},
+                                {0, 0, 0, 1, 1},
+                                {0, 1, 0, 1, 1}};
+    cout << maxAreaOfIsland(grid) << endl;
+    return 0;
+}
--- temp.cpp
+++ temp.cpp
@@ -1,68 +1,25 @@
+// C++ program to demonstrate default behaviour of
+// sort() in STL.
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    vector<int> a = {32,
-                     34,
-                     44,
-                     13,
-                     3,
-                     9,
-                     28,
-                     7,
-                     39,
-                     56,
-                     29,
-                     57,
-                     33,
-                     52,
-                     43,
-                     41,
-                     15,
-                     17,
-                     31,
-                     4,
-                     6,
-                     54,
-                     60,
-                     49,
-                     20,
-                     26,
-                     23,
-                     24,
-                     8,
-                     47,
-                     21,
-                     16,
-                     53,
-                     10,
-                     37,
-                     5,
-                     48,
-                     1,
-                     18,
-                     11,
-                     12,
-                     2,
-                     14, 95, 91, 113, 71, 96, 82, 81, 111, 98, 42, 36, 121, 68, 99, 86, 92, 90, 89, 85, 69, 103, 66, 100, 84, 109, 75, 97, 119, 118, 72, 88, 87, 65, 117, 73, 78, 80, 107, 76, 79, 114, 94, 105, 106, 101, 120, 46, 74, 70, 108};
-    sort(a.begin(), a.end());
-    // print the numbers that are not in 1-60 in the array
-    int i = 0;
-    int j = 1;
-    int count = 0;
-    while (i < a.size() && j <= 121)
+    int arr[] = {43, 69, 35, 37, 1, 52, 101, 10, 118, 120, 24, 68, 32, 22, 100, 29, 9, 25, 11, 66, 41, 78, 47, 36, 106, 21, 19, 91, 60, 12, 121, 85, 87, 63, 92, 107, 113, 102, 7, 71, 112, 6, 57, 44, 93, 42};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    /*Here we take two parameters, the beginning of the
+    array and the length n upto which we want the array to
+    be sorted*/
+    sort(arr, arr + n);
+
+    cout << "\nTk collect from rolls : \n";
+    for (int i = 0; i < n; ++i)
     {
-        if (a[i] == j)
-        {
-            i++;
-            j++;
-        }
-        else
-        {
-            // cout << j << endl;
-            count++;
-            j++;
-        }
+        cout << arr[i] << ", ";
     }
-    cout<<count;
+    cout << endl;
+    cout<<"Total given : "<<n<<endl;
+    cout<<"Total : "<<n*20<<" tk"<<endl;
+    return 0;
 }
