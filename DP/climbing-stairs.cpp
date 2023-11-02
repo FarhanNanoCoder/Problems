4042acae71f2bd0dc4d4d22232da14aa76d8282b
FarhanNanoCoder <farhan.shariar.094@gmail.com>
2023-11-03 01:33:28 +0600

    feat: climbing stairs

---
+++ DP/climbing-stairs.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int climbStairs(int n)
+{
+    if (n < 3)
+        return n;
+    int a = 1, b = 1, c;
+    while (--n)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<climbStairs(n);
+    return 0;
+}
