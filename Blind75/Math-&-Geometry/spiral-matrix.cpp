d3118866995e91c7c997a5be8d7c96b7b0bbbb21
FarhanNanoCoder <farhan.shariar.094@gmail.com>
2023-12-27 23:40:02 +0600

    feat: spiral matrix

---
+++ Blind75/Math-&-Geometry/spiral-matrix.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+    vector<int> res;
+    int t = 0, b = matrix.size() - 1, l = 0, r = matrix[0].size() - 1;
+    while (l <= r && t <= b)
+    {
+
+        for (int i = l; i <= r; i++)
+        {
+            res.push_back(matrix[t][i]);
+        }
+        t++;
+        for (int i = t; i <= b; i++)
+        {
+            res.push_back(matrix[i][r]);
+        }
+        r--;
+        if (t > b)
+            return res;
+        for (int i = r; i >= l; i--)
+        {
+            res.push_back(matrix[b][i]);
+        }
+        b--;
+        if (l > r)
+            return res;
+        for (int i = b; i >= t; i--)
+        {
+            res.push_back(matrix[i][l]);
+        }
+        l++;
+    }
+    return res;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<int> res = spiralOrder(matrix);
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+    return 0;
+}
